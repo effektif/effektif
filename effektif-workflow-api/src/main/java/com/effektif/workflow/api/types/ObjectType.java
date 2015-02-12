d4abd90dd81d7623020ddf625784b3cc4332b542
tombaeyens <mail@tombaeyens.be>
2015-02-12 19:59:47 +0100

    added maven profile for changing license headers

--- effektif-workflow-api/src/main/java/com/effektif/workflow/api/types/ObjectType.java
+++ effektif-workflow-api/src/main/java/com/effektif/workflow/api/types/ObjectType.java
@@ -1,5 +1,6 @@
-/* Copyright (c) 2014, Effektif GmbH.
- * 
+/*
+ * Copyright 2014 Effektif GmbH.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,7 +11,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. */
+ * limitations under the License.
+ */
 package com.effektif.workflow.api.types;
 
 import java.util.ArrayList;
--- effektif-workflow-impl/src/test/java/com/effektif/workflow/test/implementation/ExecutionListenerTest.java
+++ effektif-workflow-impl/src/test/java/com/effektif/workflow/test/implementation/ExecutionListenerTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2014 Heisenberg Enterprises Ltd.
+ * Copyright 2014 Effektif GmbH.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
