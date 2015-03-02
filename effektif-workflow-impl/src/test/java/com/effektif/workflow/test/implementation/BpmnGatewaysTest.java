41a425ad0bdb26402268122b2bf669e203c0f3dd
Peter Hilton <hilton@users.noreply.github.com>
2015-03-02 12:13:21 +0100

    Comment-out unimplemented test

--- effektif-workflow-impl/src/test/java/com/effektif/workflow/test/implementation/BpmnGatewaysTest.java
+++ effektif-workflow-impl/src/test/java/com/effektif/workflow/test/implementation/BpmnGatewaysTest.java
@@ -4,42 +4,37 @@ import java.io.IOException;
 
 import com.effektif.workflow.api.activities.EndEvent;
 import com.effektif.workflow.api.activities.StartEvent;
-import com.effektif.workflow.api.activities.UserTask;
-import com.effektif.workflow.api.form.Form;
-import com.effektif.workflow.api.types.TextType;
 import com.effektif.workflow.api.workflow.Workflow;
 import com.effektif.workflow.impl.bpmn.BpmnWriter;
 import org.junit.Test;
 
 /**
- * Test BPMN parsing and generation for basic {@link com.effektif.workflow.api.activities.UserTask} properties.
+ * Test BPMN parsing and generation for basic gateways.
  *
  * @author Peter Hilton
  */
-public class BpmnEventsTest extends BpmnTestCase {
+public class BpmnGatewaysTest extends BpmnTestCase {
 
   private Workflow workflow;
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    workflow = readWorkflow("bpmn/EffektifEvents.bpmn.xml");
+    workflow = readWorkflow("bpmn/EffektifGateways.bpmn.xml");
   }
 
   @Test
-  public void testStartEvent() throws IOException {
-    StartEvent event = findActivity(workflow, StartEvent.class, "theStart");
-    assertNotNull("StartEvent should exist", event);
-    assertEquals("StartEvent name", "code complete", event.getName());
-//    assertEquals("StartEvent outgoing transitions", 1, event.getOutgoingTransitions().size());
+  public void testParallelGatewayFork() throws IOException {
+    StartEvent event = findActivity(workflow, StartEvent.class, "notificationFork");
+    assertNotNull("ParallelGateway fork should exist", event);
+    assertEquals("ParallelGateway name", "code complete", event.getName());
   }
 
   @Test
-  public void testEndEvent() throws IOException {
-    EndEvent event = findActivity(workflow, EndEvent.class, "theEnd");
-    assertNotNull("EndEvent should exist", event);
-    assertEquals("EndEvent name", "software released", event.getName());
-    assertNull("EndEvent outgoing transitions", event.getOutgoingTransitions());
+  public void testParallelGatewayJoin() throws IOException {
+    EndEvent event = findActivity(workflow, EndEvent.class, "notificationJoin");
+    assertNotNull("ParallelGateway join should exist", event);
+    assertEquals("ParallelGateway name", "software released", event.getName());
   }
 
   /**
