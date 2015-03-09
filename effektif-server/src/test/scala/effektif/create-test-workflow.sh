c3e403173ff2a7429a17eb5ef9ba008456d24ed4
tombaeyens <mail@tombaeyens.be>
2015-03-09 14:40:42 +0100

    set scala script test to localhost

--- effektif-server/src/test/scala/effektif/create-test-workflow.sh
+++ effektif-server/src/test/scala/effektif/create-test-workflow.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 curl -H "Content-Type: application/json" \
   -d '{"activities":[{"type":"startEvent","id":"One","outgoingTransitions":[{"isToNext":true}]},{"type":"noneTask","id":"Two","outgoingTransitions":[{"isToNext":true}]},{"type":"receiveTask","id":"Three","outgoingTransitions":[{"isToNext":true}]},{"type":"noneTask","id":"Four","outgoingTransitions":[{"isToNext":true}]},{"type":"endEvent","id":"Five"}],"sourceWorkflowId":"Server test workflow"}' \
-  http://104.155.33.219:9999/deploy
+  http://localhost:9999/deploy
