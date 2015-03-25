34323516a88c50e5036c63681aa9791405ac2c01
tombaeyens <mail@tombaeyens.be>
2015-03-25 19:09:22 +0100

    clean up and trigger build

--- effektif-mongo/src/test/java/com/effektif/mongo/test/MongoApiTest.java
+++ effektif-mongo/src/test/java/com/effektif/mongo/test/MongoApiTest.java
@@ -38,7 +38,7 @@ public class MongoApiTest {
     
     TestSuiteHelper.run(configuration
       // use the next line if you only want to run 1 test
-       , UserTaskTest.class, "testTaskRole"
+      // , UserTaskTest.class, "testTaskRole"
       );
   }
   
