2fe116ad46356ccb2577c312757eb7a96d6008dc
Peter Hilton <hilton@users.noreply.github.com>
2015-05-05 16:39:04 +0200

    Move service task out of deprecated package

--- effektif-workflow-api/src/main/java/com/effektif/workflow/api/activities/ServiceTask.java
+++ effektif-workflow-api/src/main/java/com/effektif/workflow/api/activities/ServiceTask.java
@@ -13,10 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.effektif.workflow.api.deprecated.activities;
-
-import com.effektif.workflow.api.activities.NoneTask;
-
+package com.effektif.workflow.api.activities;
 
 /**
  * A service task is a call to code that will execute outside the service engine.
