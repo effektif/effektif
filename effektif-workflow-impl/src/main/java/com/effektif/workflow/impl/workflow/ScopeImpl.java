206db0adc1795e62f8218fd76c79e4b88a58dc01
tombaeyens <mail@tombaeyens.be>
2015-06-26 11:40:08 +0200

    fixing the properties fix

--- effektif-workflow-impl/src/main/java/com/effektif/workflow/impl/workflow/ScopeImpl.java
+++ effektif-workflow-impl/src/main/java/com/effektif/workflow/impl/workflow/ScopeImpl.java
@@ -43,7 +43,7 @@ public abstract class ScopeImpl extends Extensible {
   public List<TransitionImpl> transitions;
 
   public void parse(Scope scope, ScopeImpl parentImpl, WorkflowParser parser) {
-    this.properties = new HashMap<>(scope.getProperties());
+    this.properties = scope.getProperties()!=null ? new HashMap<>(scope.getProperties()) : null;
     this.configuration = parser.configuration;
     if (parentImpl!=null) {
       this.parent = parentImpl;
