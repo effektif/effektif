9cf3c952639093ad72e25b8fe26be8c3293776da
Peter Hilton <hilton@users.noreply.github.com>
2015-12-21 12:29:56 +0100

    Remove debug

--- effektif-workflow-impl/src/main/java/com/effektif/workflow/impl/bpmn/BpmnSchemaValidationError.java
+++ effektif-workflow-impl/src/main/java/com/effektif/workflow/impl/bpmn/BpmnSchemaValidationError.java
@@ -1,14 +1,10 @@
 package com.effektif.workflow.impl.bpmn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * API wrapper exception for BPMN schema validation errors.
  */
 public class BpmnSchemaValidationError extends RuntimeException {
 
-  private static final Logger log = LoggerFactory.getLogger(BpmnSchemaValidationError.class);
   public BpmnSchemaValidationError(Throwable cause) {
     super(cause.getMessage(), cause);
   }
