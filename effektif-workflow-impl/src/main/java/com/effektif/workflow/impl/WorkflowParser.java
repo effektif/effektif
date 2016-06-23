f3e5b1fd702e16927cf0a514f9de8c371a223f0d
MaVo <markus.vogt@thinktable.de>
2016-06-23 14:17:34 +0200

    #2994 - optimized imports

--- effektif-workflow-impl/src/main/java/com/effektif/workflow/impl/WorkflowParser.java
+++ effektif-workflow-impl/src/main/java/com/effektif/workflow/impl/WorkflowParser.java
@@ -15,18 +15,9 @@
  */
 package com.effektif.workflow.impl;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.effektif.workflow.api.Configuration;
 import com.effektif.workflow.api.condition.Condition;
+import com.effektif.workflow.api.condition.Unspecified;
 import com.effektif.workflow.api.model.RelativeTime;
 import com.effektif.workflow.api.model.WorkflowId;
 import com.effektif.workflow.api.types.DataType;
